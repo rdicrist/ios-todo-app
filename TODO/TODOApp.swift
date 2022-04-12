143d9da214f268c32861d541412f590cce5ed9d8
Ricki DiCristoforo <rdicrist@gmail.com>
2022-04-12 15:02:58 -0700

    Initial Commit

---
+++ TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by Ricki DiCristoforo on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
